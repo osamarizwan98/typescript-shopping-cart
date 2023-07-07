0b7e19c319c6f8df69ded614147f816d0e7677ec
osamarizwan98 <osamarizwan98@gmail.com>
2023-07-07 10:03:04 +0500

    Nabvbar Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { Container } from 'react-bootstrap'
+import {Routes, Route} from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Store from './pages/Store'
+import Navbar from './components/Navbar'
+
 function App() {
 
   return (
     <>
-      <h1>Hi</h1>
+    <Navbar />
+    <Container>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/store' element={<Store />} />
+      </Routes>
+    </Container>
     </>
   )
 }
