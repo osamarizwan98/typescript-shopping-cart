f1c4e210dacc98194e4aa9b53512f4075a5fe1c9
osamarizwan98 <osamarizwan98@gmail.com>
2024-07-10 22:57:57 +0500

    added store items

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,21 @@ import About from './pages/About'
 import Home from './pages/Home'
 import Store from './pages/Store'
 import Navbar from './components/Navbar'
+import { ShoppingCartProvider } from './context/shoppingCartContext'
 
 function App() {
 
   return (
-    <>
+    <ShoppingCartProvider>
     <Navbar />
-    <Container>
+    <Container className="mb-4">
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/store' element={<Store />} />
       </Routes>
     </Container>
-    </>
+    </ShoppingCartProvider>
   )
 }
 
